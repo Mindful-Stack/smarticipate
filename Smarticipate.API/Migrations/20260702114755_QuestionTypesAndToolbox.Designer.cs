ff4a42c68de78331cf80e78cdd4d5777b4a75d2d
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2026-07-02 14:08:25 +0200

    Add QuestionTypesAndToolbox EF migration
    Drop the retired Questions table and reshape Responses (ActivationId,
    ParticipantKey, SubmittedAt, NumericValue, TextValue). Add the
    QuestionDefinitions, QuestionOptions, QuestionActivations, and
    ResponseSelections tables with the OwnerUserId FK, the jsonb config column,
    and the participant and selection dedup unique indexes.

--- Smarticipate.API/Migrations/20260702114755_QuestionTypesAndToolbox.Designer.cs
+++ Smarticipate.API/Migrations/20260702114755_QuestionTypesAndToolbox.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Smarticipate.API.Data.Identity;
@@ -11,9 +12,11 @@ using Smarticipate.API.Data.Identity;
 namespace Smarticipate.API.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260702114755_QuestionTypesAndToolbox")]
+    partial class QuestionTypesAndToolbox
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
